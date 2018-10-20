5f02deb0b088c05f6368cefb4572a3ac33f101b1
Kristo <vasschris69@gmail.com>
2018-10-20 16:42:26 +0300

    First StudentApp written in C#
    Shows students name, grade and date.

---
+++ StudentApp/StudentApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>();
+            var createdDate = new List<DateTime>();
+            
+            //creating new students
+            for(int i=0;i<2;i++)
+            {
+                string firstName=null;
+                string secondName = null;
+                int numberGrades = 7;
+                int[] grades = new int[numberGrades]; //each student has 7 grades
+
+                Console.WriteLine("Enter first name:");
+                firstName = Console.ReadLine();
+                Console.WriteLine("Enter second name:");
+                secondName = Console.ReadLine();
+
+                //input grades
+                Console.WriteLine("Enter grades:");
+                for (int j = 0; j < numberGrades; j++)
+                {
+                    int grade = int.Parse(Console.ReadLine());
+                    if (grade < 11 && grade > -1)
+                        grades[j] = grade;
+                    else
+                        throw new ArgumentException("Grade is invalid");
+                }
+
+                //creating a new student
+                students.Add(new Student(firstName, secondName, grades));
+
+                createdDate.Add(DateTime.Now);
+                Console.WriteLine("Student created!");
+                Console.WriteLine();
+            }
+            Console.Clear();
+
+            //display current students, grades and time of creation
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine(students[i]);
+                Console.WriteLine("Created on " + createdDate[i]);
+                Console.WriteLine();
+            }
+
+            //console doesn't close
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StudentApp/StudentApp/Student.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace StudentApp
+{
+    
+    class Student
+    {
+        //members
+        private string firstName;
+        private string secondName;
+        const int numberOfGrades = 7;
+        private int[] studentGrade = new int[numberOfGrades];
+
+        //default constructor
+       public Student()
+        {
+            this.firstName = null;
+            this.secondName = null;
+            this.studentGrade = null;
+        }
+        //Main constructor
+       public Student(string firstName, string secondName, int[] studentGrade)
+        {
+            this.firstName = firstName;
+            this.secondName = secondName;
+            this.studentGrade = studentGrade;
+        }
+
+        //getters
+        public string getFirstName()
+        {
+            return this.firstName;
+        }
+        public string getSecondName()
+        {
+            return this.secondName;
+        }
+        
+        //method to display grades
+        public void displayGrades()
+        {
+            Console.Write("Grades: ");
+            foreach(var grade in studentGrade)
+                Console.Write(grade);
+        }
+
+        //operator overloading
+        private string getStudent="";
+        public override string ToString()
+        {
+            Console.WriteLine("First name: " + getFirstName());
+            Console.WriteLine("Second name: " + getSecondName());
+            displayGrades();
+            return this.getStudent.ToString();
+        }
+    }
+}
